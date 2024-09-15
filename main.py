a80fc44716f3cedeea68a440672cd6204c35aca1
Sammy0s <Samantha.Yengst@gmail.com>
2024-09-15 18:31:13 +0000

    Very Basic Skeleton for Chatbot Responses in Python
    Removed junk code and solved issue of any lines after 3 not running

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-print("Hello")
-hi = input("Hello! ")
-print(hi)
-
 #Okay.. Chatbot project. I need to give the user some stuff to do.abs
 # For now, it's probably fine to use print functions and stuff. I don't need a fancy UI
 
@@ -12,8 +8,9 @@ print("Welcome to the chatbot!")
 name = input("What is your name? ")
 print(f"Hello, {name}!")
 # Ask the user how it can help them
+option = 0
 while option != 1 and option != 2 and option != 3:
-  option = input("How can I assist you today? (1)Chat, (2)Math, (3) Exit:")  
+  option = int(input("How can I assist you today? (1)Chat, (2)Math, (3) Exit: "))  
 # Allow the user to choose from a menu/list of options on how they can continue the conversation
 if (option==1):
   print("You chose Chat.")
