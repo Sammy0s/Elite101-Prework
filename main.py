4097dc0c5b8afe64347e333e8f7d2a4171e0896e
Sammy0s <Samantha.Yengst@gmail.com>
2024-09-15 18:55:51 +0000

    Loop option menu until user exits program.
    Put menu in a while loop that breaks when user exits program.
    
    Not the most elegant solution in the world, but that's okay for now.

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ print("Welcome to the chatbot!")
 #Collect the user’s name and age
 name = input("What is your name? ")
 print(f"Hello, {name}!")
+age = input("How old are you? ")
+print(f"You are {age} years old.")
 # Ask the user how it can help them
-option = 0
-while option != 1 and option != 2 and option != 3:
-  option = int(input("How can I assist you today? (1)Chat, (2)Math, (3) Exit: "))  
-# Allow the user to choose from a menu/list of options on how they can continue the conversation
-if (option==1):
-  print("You chose Chat.")
-elif (option ==2):
-  print("You chose Math.")
-elif (option ==3):
-  print("You chose Exit.")
-  print(f"Goodbye, {name}!")
+# In a while loop so continues until user exits
+while True:
+  option = 0
+  while option != 1 and option != 2 and option != 3:
+    option = int(input("How can I assist you today? (1)Chat, (2)Math, (3) Exit: "))  
+  # Allow the user to choose from a menu/list of options on how they can continue the conversation
+  if (option==1):
+    print("You chose Chat.")
+  elif (option ==2):
+    print("You chose Math.")
+  elif (option ==3):
+    print("You chose Exit.")
+    print(f"Goodbye, {name}!")
+    break
